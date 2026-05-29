81ab8a1629f80fb46509cd11e315bd6164d9aa0f
batman230-sai <22341A0230@gmrit.edu.in>
2026-05-29 20:47:48 +0530

    Feat: Logger file developed

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+import os
+from datetime import datetime
+
+# Define the log file name based on current time
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create the logs directory path in the root folder
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure the logging format
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
