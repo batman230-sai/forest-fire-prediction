9371d5b1956ac4043a1d1485c8e8c7b847414945
batman230-sai <22341A0230@gmrit.edu.in>
2026-05-30 20:33:09 +0530

    feat: DataIngestion file setup

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -5,7 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+#dataset loading
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -15,3 +15,12 @@ class DataIngestionConfig:
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
+
+if __name__ == "__main__":
+    # 1. Initialize the ingestion class
+    obj = DataIngestion()
+    
+    # 2. Call the action method (once you write it)
+    # train_data, test_data = obj.initiate_data_ingestion()
+    
+    print("Data Ingestion setup is executing...")
