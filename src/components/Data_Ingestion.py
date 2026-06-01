eb48f7492411d3ee81293d58403dc1e1e68249c4
batman230-sai <22341A0230@gmrit.edu.in>
2026-06-01 09:33:56 +0530

    Feat: DataIngestion file with object create

--- src/components/Data_Ingestion.py
+++ src/components/Data_Ingestion.py
@@ -1,11 +1,14 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-#dataset loading
+from src.logger import logging
+from src.exception import CustomException
+
+from src.components.Data_Transformation import DataTransformation
+from src.components.Model_Trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
@@ -16,11 +19,39 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
+    def initiate_data_ingestion(self):
+        logging.info("Starting Data Ingestion Method")
+        try:
+            # Point this to your raw dataset
+            df = pd.read_csv("notebooks/Algerian_forest_fires_dataset_UPDATE.csv")
+            logging.info("Read the dataset successfully")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Initiating Train-Test Split")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data Ingestion completed successfully")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+# Triggering the pipeline
 if __name__ == "__main__":
-    # 1. Initialize the ingestion class
     obj = DataIngestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
     
-    # 2. Call the action method 
-    # train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
     
-    print("Data Ingestion setup is executing...")
+    model_trainer = ModelTrainer()
+    best_score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    print(f"Pipeline Completed! Best R2 Score: {best_score}")
