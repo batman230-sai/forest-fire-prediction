faf34c2561465501c3e384676725cda4004ead40
batman230-sai <22341A0230@gmrit.edu.in>
2026-06-01 09:44:38 +0530

    Feat: Created get_transformer_object class to build pickle file

--- src/components/Data_Transformation.py
+++ src/components/Data_Transformation.py
@@ -13,3 +13,18 @@ from src.utils import save_object
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path: str = os.path.join('artifacts', 'preprocessor.pkl') 
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+    def get_transformer_object(self):
+        """Creating scaling pipeline"""
+        try:
+            numerical_columns=['Temperature', 'RH', 'Ws', 'Rain', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'Classes']
+            num_pipeline = Pipeline(steps=[("scaler", StandardScaler())])
+            preprocessor = ColumnTransformer(transformers=[("num_pipeline", num_pipeline, numerical_columns)])
+            return preprocessor
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+
+    
