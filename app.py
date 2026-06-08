6ff56e11ff0d8ae77aee76dc2bf2efde5cb5315f
batman230-sai <22341A0230@gmrit.edu.in>
2026-06-08 20:22:43 +0530

    API Template Create

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 from fastapi import FastAPI
-app=FastAPI()
-@app.get("/")
-def hello():
-    return {"message":"Hello World"}
+from pydantic import BaseModel
+
+# Initialize the app
+app = FastAPI(title="Forest Fire Prediction API")
+# Pydantic Model 
+class ModelFeatures(BaseModel):
+    """
+    Reads input data and validates it automatically.
+    """
+
+# GET Request: About Section
+@app.get("/about")
+def about_me():
+    """
+    Displays professional information and interactive links.
+    """
+
+# POST Request: Prediction Endpoint
+# ---------------------------------------------------------
+@app.post("/predict")
+def predict(data: ModelFeatures):
+    """
+    Collects input data, runs the ML model, and sends back the prediction.
+    """
