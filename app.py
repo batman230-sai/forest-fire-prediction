162aed677420c4b53c37439de402f8f897606d16
batman230-sai <22341A0230@gmrit.edu.in>
2026-06-09 20:05:40 +0530

    Data Validation using Pydantic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 # Initialize the app
 app = FastAPI(title="Forest Fire Prediction API")
@@ -15,16 +15,16 @@ def about_me():
     """
     Displays professional information and interactive links.
     """
-    Temperature: int
-    RH: int
-    Ws: int
-    Rain: int
-    FFMC: float
-    DMC: float
-    DC: float
-    ISI: float
-    BUI: float
-    Classes: int
+    Temperature: int=Field(ge=-10,le=60,description="Temperature in Celsius")
+    RH: int=Field(ge=10,le=100,description="Relative Humidity in %")
+    Ws: int=Field(ge=0,le=100,description="Wind Speed in km/h")
+    Rain: int=Field(ge=0,description="Rainfall in mm")
+    FFMC: float=Field(ge=0.0,le=100.0,description="Fine Fuel Moisture Code")
+    DMC: float=Field(ge=0.0,description="Duff Moisture Code")
+    DC: float=Field(ge=0.0,description="Drought Code")
+    ISI: float=Field(ge=0.0,description="Initial Spread Index")
+    BUI: float=Field(ge=0.0,description="Build Up Index")
+    Classes: int=Field(ge=0,le=1)
 
 # POST Request: Prediction Endpoint
 @app.post("/predict")
