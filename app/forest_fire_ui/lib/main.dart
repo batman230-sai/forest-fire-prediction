1514e717a9a33523fe620b23465c6da2f082bd95
batman230-sai <22341A0230@gmrit.edu.in>
2026-06-18 20:31:27 +0530

    Flutter UI

--- app/forest_fire_ui/lib/main.dart
+++ app/forest_fire_ui/lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // Notice that the counter didn't reset back to zero; the application
         // state is not lost during the reload. To reset the state, use hot
         // restart instead.
         //
